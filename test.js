305e3ad07b2c103c669e709083bd5a787a7eb8d2
zhaoshengjun <zhao.shengjun@gmail.com>
2016-06-23 13:10:33 +1000

    dispose called

--- test.js
+++ test.js
@@ -8,6 +8,7 @@ var source = Rx.Observable.create(function (observer) {
   }, 1000);
   console.log('started!');
   return function () {
+    console.log('dispose called!');
     clearTimeout(id);
   }
 });
