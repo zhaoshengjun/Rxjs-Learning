95f0014cc293a4d9df18312142355dd4ba82f22a
zhaoshengjun <zhao.shengjun@gmail.com>
2016-06-23 13:07:52 +1000

    setTimeout

--- test.js
+++ test.js
@@ -1,8 +1,12 @@
 /// <reference path="rx.d.ts" />
 console.log("Let's begin!");
 var source = Rx.Observable.create(function (observer) {
-  observer.onNext(42);
-  observer.onCompleted();
+  setTimeout(function () {
+    console.log('timeout hit!')
+    observer.onNext(42);
+    observer.onCompleted();
+  }, 1000);
+  console.log('started!');
 });
 
 var sub = source.subscribe(function (x) {
